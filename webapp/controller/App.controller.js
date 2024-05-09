05f670119bc33148e90322de9d0944949f603303
Jingwei Wang <jingwei.wang@convista.com>
2024-05-09 13:04:48 +0000

    Part_5 Controller with BTP

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+      return BaseController.extend("project1.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+      
+    }
+  );
+  
