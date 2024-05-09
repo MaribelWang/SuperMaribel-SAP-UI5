ddb25880c67d34772cd103635714fb44fe0b73d0
Jingwei Wang <jingwei.wang@convista.com>
2024-05-09 14:23:32 +0000

    Part_7 JSON model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,16 +1,29 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, MessageToast) {
-        "use strict";
+    "sap/m/MessageToast", //library m, module MessageToast
+    "sap/ui/model/json/JSONModel",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("project1.controller.App", {
+      onInit: function () {
+        //set a data model on the view
+        var oData = {
+          recipient: {
+            name: "Jingwei",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
 
-        return Controller.extend("project1.controller.App", {
-            onSayHiButtonPress: function() {
-              MessageToast.show('Hello there');
-            }
-          });
+      onSayHiButtonPress: function () {
+        MessageToast.show("Hello there");
+      },
     });
+  }
+);
