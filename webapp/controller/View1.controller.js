e059ecd782e9ca9a63cb37f1bd720c674242e3e9
Jingwei Wang <jingwei.wang@convista.com>
2024-05-10 09:08:03 +0000

    Part_8 Translatable Texts i18n

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,11 +3,12 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast", //library m, module MessageToast
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("project1.controller.App", {
       onInit: function () {
@@ -19,10 +20,24 @@ sap.ui.define(
         };
         var oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+        //set i18n model on view
+        var i18nModel = new ResourceModel({
+          bundleName: "project1.i18n.i18n",
+          suportedLocales: [""],
+          fallbackLocales: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
 
       onSayHiButtonPress: function () {
-        MessageToast.show("Hello there");
+        //read mesg from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        //Show message
+        MessageToast.show(sMsg);
       },
     });
   }
