94bfbcd0876d914f6ca580ad923dde03bdbac02c
Jingwei Wang <jingwei.wang@convista.com>
2024-05-13 14:12:32 +0000

    error with router

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,21 +1,41 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast", //library m, module MessageToast(pop-up)
+    "sap/m/MessageToast", //library m, module MessageToast
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("project1.controller.App", {
+      onInit: function () {
+        //set a data model on the view
+        var oData = {
+          recipient: {
+            name: "Jingwei",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+        //set i18n model on view
+        var i18nModel = new ResourceModel({
+          bundleName: "project1.i18n.i18n",
+          suportedLocales: [""],
+          fallbackLocales: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
+      },
+
       onSayHiButtonPress: function () {
         //read mesg from i18n model
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
           .getModel()
           .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
         //Show message
         MessageToast.show(sMsg);
       },
